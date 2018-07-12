a6f66ce5d680591bf2f603660ef36c23d743fc72
Mayank Verma <mayank.verma@nviz.com>
2018-07-12 22:14:29 +0530

    rabbit mq subscriber working using spring

---
+++ src/main/java/com/rabbitmq/java/client/pubsub_sample/springrest/RabbitMQSubscriber.java
@@ -0,0 +1,13 @@
+package com.rabbitmq.java.client.pubsub_sample.springrest;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RabbitMQSubscriber {
+	
+	@RabbitListener(queues="${test.rabbitmq.queue}")
+    public void recievedMessage(Employee msg) {
+        System.out.println("Recieved Message: " + msg);
+    }
+}
